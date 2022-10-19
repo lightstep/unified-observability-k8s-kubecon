5a5c817b3c0359ad6ed98e9cb894518830f6b99a
Adriana Villela <indycomp0t1@gmail.com>
2022-10-19 15:39:04 -0400

    Remove dependency on itscontained/raw helm chart, thanks to using depends_on for data objects. Wheeee

--- k8s-cluster-with-otel-demo/terraform/modules/otel_demo_app/main.tf
+++ k8s-cluster-with-otel-demo/terraform/modules/otel_demo_app/main.tf
@@ -1,31 +1,52 @@
 # Create namespace and k8s secret for LS access token
 # Reference: https://github.com/hashicorp/terraform-provider-kubernetes/issues/1380#issuecomment-962058148
-resource "helm_release" "resources" {
-  name       = "external-secrets-cluster-store"
-  chart      = "../itscontained/raw"
-  values = [
-    <<-EOF
-    resources:
-      - apiVersion: v1
-        kind: Namespace
-        metadata:
-          name: ${var.otel_demo_namespace}
+# resource "helm_release" "resources" {
+#   name       = "external-secrets-cluster-store"
+#   chart      = "../itscontained/raw"
+#   values = [
+#     <<-EOF
+#     resources:
+#       - apiVersion: v1
+#         kind: Namespace
+#         metadata:
+#           name: ${var.otel_demo_namespace}
 
-      - apiVersion: v1
-        kind: Secret
-        metadata:
-          name: otel-collector-secret
-          namespace: ${var.otel_demo_namespace}
-        data:
-          LS_TOKEN: ${base64encode(var.ls_access_token)}
-        type: "Opaque"
-    EOF
-  ]
+#       - apiVersion: v1
+#         kind: Secret
+#         metadata:
+#           name: otel-collector-secret
+#           namespace: ${var.otel_demo_namespace}
+#         data:
+#           LS_TOKEN: ${base64encode(var.ls_access_token)}
+#         type: "Opaque"
+#     EOF
+#   ]
+# }
+
+resource "kubernetes_namespace_v1" "otel_demo_ns" {
+  metadata {
+    name = var.otel_demo_namespace
+  }
+}
+
+
+resource "kubernetes_secret_v1" "ls_access_token" {
+  depends_on = [kubernetes_namespace_v1.otel_demo_ns]
+  metadata {
+    name = "otel-collector-secret"
+    namespace = var.otel_demo_namespace
+  }
+  data = {
+    LS_TOKEN = var.ls_access_token
+  }
+
+  type = "Opaque"
 }
 
 # Deploy demo app
 resource "helm_release" "otel_demo_app" {
-  depends_on = [helm_release.resources]
+  # depends_on = [helm_release.resources]
+  depends_on = [kubernetes_secret_v1.ls_access_token]
   name             = "otel-demo-app"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-demo"
--- k8s-cluster-with-otel-demo/terraform/providers.tf
+++ k8s-cluster-with-otel-demo/terraform/providers.tf
@@ -14,6 +14,10 @@ terraform {
       version = ">=1.70.0"
     }
 
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
   }
 
   # required_version = ">= 0.14"
@@ -40,6 +44,14 @@ data "google_container_cluster" "primary" {
   location = var.region
 }
 
+
+provider "kubernetes" {
+  host  = data.google_container_cluster.primary.endpoint
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  )
+}
 
 provider "helm" {
   kubernetes {
